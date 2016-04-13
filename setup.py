c25e69f1ab6a9bcbd1133f5ff37db98c487cb4d2
William Schoenell <wschoenell@gmail.com>
2016-04-13 19:10:15 -0300

    added pymodbus as requirement

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ setup(
     version='0.0.1',
     packages=['chimera_commandersk', 'chimera_commandersk.instruments', 'chimera_commandersk.instruments.skdrv',
               'chimera_commandersk.controllers'],
+    install_requires='pymodbus',
     scripts=[],
     url='http://github.com/astroufsc/chimera-commandersk',
     license='GPL v2',
     author='Salvador Agati',
-    author_email='',
+    author_email='wschoenell@gmail.com',
     description='Emerson Control Techniques COMMANDER SK driver for the chimera observatory control system.'
 )
